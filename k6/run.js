fbc21daae80294da16f27f1ca9322995c46593d5
CI/CD <>
2024-03-27 16:37:18 -0300

    ci: 🎡 k6 test

---
+++ k6/run.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+export default function () {
+  http.get('http://localhost:8080/health');
+  sleep(1);
+}
